a7218baed0ac333b87f7ae60030f471b30c3fad7
Zaw Ye <zawye@Kaungs-MacBook-Pro.local>
2019-09-03 16:28:58 +0630

    update

--- wp-content/themes/transportex/js/transportex.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-
-	/* Slider section */
-	wp.customize.section( 'sidebar-widgets-sidebar-slider' ).panel( 'homepage_setting' );
-	wp.customize.section( 'sidebar-widgets-sidebar-slider' ).priority( '1' );
-	
-});
